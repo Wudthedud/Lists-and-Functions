5f070000cf965549d8d6b094fac23b0e14d25131
wudthedud <wud@middleton.school.nz>
2024-02-14 14:16:20 +1300

    Update MGS_Childcare.py

---
+++ MGS Childcare/MGS_Childcare.py
@@ -0,0 +1,47 @@
+namesList = []
+repeat = True
+
+def checkin():
+    name = input("Enter your child's name: ").title().strip()
+    namesList.append(name)
+    print(f"{name} has been added to the roll\n")
+    print("-" * 30)
+
+def checkout():
+    name = input("Enter your child's name: ").title().strip()
+    try:
+        namesList.remove(name)
+        print(f"{name} has been removed from the roll")
+    except:
+        print(f"{name} is not in the roll")
+        print("-" * 30)
+
+
+def cost():
+    hours = input("Enter amount of hours: ")
+    while hours.isdigit == False:
+        print("Please enter the number of hours as an integer! ")
+        hours = input("Enter amount of hours: ")
+
+def roll():
+    pass
+
+def main():
+    print("Welcome to MGS childcare, please select and option to continue: ")
+    while repeat == True:
+        mode = input("[1] Drop off a child\n[2] Pick up a child\n[3] Calculate cost\n[4] Print roll\n[5] Exit the syetem\n")
+        print("-" * 30)
+        if mode == "1": 
+            checkin()
+        elif mode == "2":
+            checkout()
+        elif mode == "3":
+            cost()
+        elif mode == "4":
+            roll()
+        elif mode == "5":
+            print("Thank you for visiting MGS Childcare")
+            break
+        else:
+            print("Please enter a number between 1-5")
+main()
